a9be5dff031e03d5a718127737cf8cd73876167b
Kyle Mandli <kyle.mandli@gmail.com>
2017-10-13 18:39:58 -0400

    Slide dtopo now working)

--- make_dtopo.py
+++ make_dtopo.py
@@ -7,56 +7,63 @@ import clawpack.geoclaw.topotools as tt
 import clawpack.geoclaw.dtopotools as dt
 
 def transform(x, y, theta=0.0):
-
-    A = numpy.eye(2)
-    return numpy.dot(A, numpy.array([x, y]))
-
+    return (x * numpy.cos(theta) + y * numpy.sin(theta), 
+            x * numpy.sin(theta) - y * numpy.cos(theta))
 
 def slide_topo(x, y, t):
 
-    x_c = 492500
+    x_start, y_start = 492500, 3085500
     slide_speed = 50
-    y_start = 3085500
-    y_c = y_start + slide_speed * t
-    xi_c, zeta_c = transform(x_c, y_c)
-    x_start, zeta_start = transform(0.0, y_start)
+    slide_max_length = 1000.0
+    theta = numpy.pi / 3.0
     sigma = 100.0
     A = 10.0
 
     # Transform coordinates
-    xi, zeta = transform(x, y)
+    eta, zeta = transform(x, y, theta)
+    eta_start, zeta_start = transform(x_start, y_start, theta)
 
+    # Derive lengths in eta-zeta coordinate system
+    eta_c = eta_start + slide_speed * t
+    zeta_c = zeta_start
+    if eta_c - eta_start > slide_max_length:
+        eta_start = eta_c - slide_max_length
+    else:
+        eta_end = eta_start
+
+    # Compute dtopo
     slide = numpy.zeros(x.shape)
 
     # front
-    slide += A * numpy.exp(-((xi - xi_c)**2 + (zeta - zeta_c)**2) / sigma**2) * (zeta - zeta_c > 0.0)
+    slide += A * numpy.exp(-((eta - eta_c)**2 + (zeta - zeta_c)**2) / sigma**2) * (eta > eta_c)
 
     # center
-    slide += A * numpy.exp(-((xi - xi_c)**2) / sigma**2) * (zeta - zeta_c <= 0.0) * (zeta - zeta_start >= 0.0)
+    slide += A * numpy.exp(-((zeta - zeta_c)**2) / sigma**2) * (eta  <= eta_c) * (eta >= eta_start)
 
     # back
-    slide += A * numpy.exp(-((xi - xi_c)**2 + (zeta - zeta_start)**2) / sigma**2) * (zeta - zeta_start < 0.0)
+    slide += A * numpy.exp(-((eta - eta_start)**2 + (zeta - zeta_start)**2) / sigma**2) * (eta < eta_start)
 
     return slide
 
 if __name__ == "__main__":
-
     # Create dtopo
     dtopo_path = "./topo/imja_slide.tt3"
     dtopo = dt.DTopography()
     dtopo.x = numpy.linspace(491000, 494500, 100)
     dtopo.y = numpy.linspace(3085200, 3086800, 100)
-    X, Y = numpy.meshgrid(dtopo.x, dtopo.y)
-    T = numpy.linspace(0, 25, 6)
-    dtopo.Z = numpy.empty((dtopo.x.shape[0], dtopo.y.shape[0], T.shape[0]))
-    for (i, t) in enumerate(T):
-        dtopo.Z[:, :, i] = slide_topo(X, Y, t)
+    dtopo.X, dtopo.Y = numpy.meshgrid(dtopo.x, dtopo.y)
+    dtopo.times = numpy.linspace(0, 35, 8)
+    dtopo.dZ = numpy.empty((dtopo.times.shape[0], dtopo.x.shape[0], 
+                                                  dtopo.y.shape[0]))
+    for (i, t) in enumerate(dtopo.times):
+        dtopo.dZ[i, :, :] = slide_topo(dtopo.X, dtopo.Y, t)
 
-    for t in T:
+    for i in range(dtopo.times.shape[0] - 1, -1, -1):
         fig = plt.figure()
         axes = fig.add_subplot(1, 1, 1)
-        axes.pcolor(dtopo.X, dtopo.Y, dtopo.dZ_at_t(t))
-        axes.set_title("t = %s" % str(t))
+        axes.pcolor(dtopo.x, dtopo.y, dtopo.dZ_at_t(dtopo.times[i]))
+        # axes.pcolor(X, Y, slide_topo(X, Y, T[i]))
+        axes.set_title("t = %s" % str(dtopo.times[i]))
 
     # topo.plot(contour_levels=[5000], limits=[4750, 5500])
     plt.show()
